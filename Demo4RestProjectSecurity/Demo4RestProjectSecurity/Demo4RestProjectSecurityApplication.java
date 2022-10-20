b0e241889ce83e9fca7cda419adeea70267f67f1
unknown <cogjava3300@iiht.tech>
2022-10-20 10:42:24 +0530

    First Commit

---
+++ Demo4RestProjectSecurity/Demo4RestProjectSecurity/Demo4RestProjectSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.spring.demo.Demo4RestProjectSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo4RestProjectSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo4RestProjectSecurityApplication.class, args);
+	}
+
+}
---
+++ Demo4RestProjectSecurity/src/test/java/com/spring/demo/Demo4RestProjectSecurity/Demo4RestProjectSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.demo.Demo4RestProjectSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo4RestProjectSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
